61f50e2daded32657ee723a6f99d567f8728d252
Alexwell <alexl88v@gmail.com>
2019-06-22 21:12:02 +0300

    Front-end update

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='templates',
+            template_folder='templates')
 app.config.from_object(Config)
 
 from app import routes, models, errors

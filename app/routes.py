1c009a57f3c14a338245b55d7e5f3b2de57eecc7
Alexwell <alexl88v@gmail.com>
2019-06-22 22:25:18 +0300

    AJAX test

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, url_for
+from flask import render_template, url_for, request, jsonify
 from app import app
 
 
@@ -7,3 +7,20 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    for i in request.form:
+        print(f'{i} ===> {request.form[i]}')
+
+    response_str = request.form['testParamStr'] + '! Hello from back end!'
+    response_int = int(request.form['testParamNumber']) + 71
+    response_float = float(request.form['testParamNumber']) + 71.8
+    response_bull = not request.form['testParamBull']
+
+    return jsonify({'response_str': response_str,
+                    'response_int': response_int,
+                    'response_float': response_float,
+                    'response_bull': response_bull
+                    })
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,27 @@
+import $ from "jquery";
 import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 
+
+$(document).ready(function () {
+    $('#signIn').click(function () {
+        console.log('test');
+        testRequest();
+    });
+
+    function testRequest() {
+        $.post('/test', {
+            testParamStr: 'Hello from front end',
+            testParamNumber: 29,
+            testParamBull: true
+        }).done(function (response) {
+            for (let i in response) {
+                console.log(i + ' ===> ' + response[i]);
+            }
+        }).fail(function () {
+            console.log('No response')
+        })
+    }
+});
 

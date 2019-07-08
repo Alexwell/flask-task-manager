a76cbe1458db81c5cc9aa232624440b2ff9a3140
Alexwell <alexl88v@gmail.com>
2019-07-08 20:35:02 +0300

    Create app hotfix

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='templates',
-            template_folder='templates')
-app.config.from_object(Config)
-cors = CORS(app, resources={r'/json/*': {'origins': '*'}})
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+# app = Flask(__name__,
+#             static_url_path='',
+#             static_folder='templates',
+#             template_folder='templates')
+# app.config.from_object(Config)
+# cors = CORS(app, resources={r'/json/*': {'origins': '*'}})
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+# login.login_message = _l('Please log in to access this page.')
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+    return app
+
 
 from app import routes, models, errors
